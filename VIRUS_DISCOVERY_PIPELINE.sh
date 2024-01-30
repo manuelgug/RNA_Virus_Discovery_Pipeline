961f8242a1c787373a8ce73f68ac94513776d952
manuelgug <36465310+manuelgug@users.noreply.github.com>
2024-01-30 16:49:57 +0100

    add prokka bit

--- VIRUS_DISCOVERY_PIPELINE.sh
+++ VIRUS_DISCOVERY_PIPELINE.sh
@@ -220,3 +220,20 @@ do
 done
 
 rm *membership.tsv
+
+
+#---ANNOTATION---
+
+#!/bin/bash -v
+
+source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
+conda activate /home/ubuntu/miniconda3/envs/prokka_vir
+
+for f in *_ALL_VIRAL_CONTIGS-final
+do
+
+prefix=$(basename $f _ALL_VIRAL_CONTIGS-final)
+
+prokka  --outdir ${prefix}_prokka-viral-contigs --cpus 0 --norrna --notrna --proteins ../viral_proteins_ncbi_19-09-22_db/viral_proteins_ncbi.fasta "$f"/${prefix}_ALL_VIRAL_CONTIGS_SEQS.fasta
+
+done
